46a3adff5e44d061ee914e1782cb76d58a66af00
norjamillekasan <jamclong0328@gmail.com>
2022-09-23 19:15:40 +0800

    floor

--- database/seeders/FloorAndRoomSeeder.php
+++ database/seeders/FloorAndRoomSeeder.php
@@ -21,17 +21,17 @@ class FloorAndRoomSeeder extends Seeder
                 'branch_id' => 1,
                 'number' => $i,
             ]);
-            for ($j = 1; $j <= 10; $j++) {
-                // random room type
-                $types = ['1', '2', '3', '4'];
-                $type = $types[array_rand($types)];
-                $room = Room::create([
-                    'number' => $room_number++,
-                    'floor_id' => $floor->id,
-                    'type_id' => $type,
-                    'room_status_id' => 1,
-                ]);
-            }
+            // for ($j = 1; $j <= 10; $j++) {
+            //     // random room type
+            //     $types = ['1', '2', '3', '4'];
+            //     $type = $types[array_rand($types)];
+            //     $room = Room::create([
+            //         'number' => $room_number++,
+            //         'floor_id' => $floor->id,
+            //         'type_id' => $type,
+            //         'room_status_id' => 1,
+            //     ]);
+            // }
         }
     }
 }
