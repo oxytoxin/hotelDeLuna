61090f998742be449b36186640a24c20fd2e8cd6
norjamillekasan <jamclong0328@gmail.com>
2022-10-03 22:08:53 +0800

    Update check-out.blade.php

--- resources/views/front-desk/check-out.blade.php
+++ resources/views/front-desk/check-out.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <x-layout.frontdesk>
         <x-page-layout title="Check Out">
-            @livewire('front-desk.check-out')
+            @livewire('front-desk.check-out-guest')
         </x-page-layout>
     </x-layout.frontdesk>
 @endsection
