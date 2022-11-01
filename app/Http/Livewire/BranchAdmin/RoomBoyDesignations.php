323153b9943a78c0c5207e6ec059dd4e4b997885
norjamillekasan <jamclong0328@gmail.com>
2022-11-01 14:23:25 +0800

    roomBoyFixed

--- app/Http/Livewire/BranchAdmin/RoomBoyDesignations.php
+++ app/Http/Livewire/BranchAdmin/RoomBoyDesignations.php
@@ -3,11 +3,132 @@
 namespace App\Http\Livewire\BranchAdmin;
 
 use Livewire\Component;
+use App\Models\{Designation,RoomBoy,Floor,User};
+
+use Livewire\WithPagination;
+use WireUi\Traits\Actions;
+use App\Traits\WithCaching;
 
 class RoomBoyDesignations extends Component
 {
+    use WithPagination, Actions, WithCaching;
+
+    public $designation;
+
+    public $manageDesignationModal = false;
+
+    public $searchRoomBoy;
+
+    public $roomBoysList = [];
+
+    public $floors = [];
+
+    public $floors_filter = [];
+
+    public $filter = [
+        'floor_id'=>''
+    ];
+
+    public $room_boy;
+
+    public $search;
+
+    protected $validationAttributes = [
+        'floor_id'=>'Floor'
+    ];
+
+    public function rules()
+    {
+        return [
+            'designation.floor_id' => 'required',
+            'designation.room_boy_id' => 'required',
+            'designation.current' => 'required',
+        ];
+    }
+
+    public function updatedSearchRoomBoy()
+    {
+        $this->useCacheRows();
+        $this->roomBoysList = User::where('branch_id', auth()->user()->branch_id)
+            ->where('role_id', 5)
+            ->where('name', 'like', '%' . $this->searchRoomBoy . '%')
+            ->orWhere('email', 'like', '%' . $this->searchRoomBoy . '%')
+            ->with(['room_boy'])
+            ->get(['id', 'name']);
+    }
+
+    public function getRoomBoysQueryProperty()
+    {
+        return RoomBoy::whereHas('user', function ($query) {
+            $query->where('branch_id', auth()->user()->branch_id);
+        })->with(['user', 'designations'=>function($query){
+            $query->where('current',1)
+            ->with(['floor']);
+        }]);
+    }
+
+    public function getRoomBoysProperty()
+    {
+        if ($this->search) {
+            $this->roomBoysQuery->whereHas('user', function ($query) {
+                $query->where('name', 'like', '%' . $this->search . '%')
+                    ->orWhere('email', 'like', '%' . $this->search . '%');
+            });
+        }
+
+        if ($this->filter['floor_id']) {
+            $this->roomBoysQuery->whereHas('designations', function ($query) {
+                $query->where('floor_id', $this->filter['floor_id'])
+                    ->where('current', 1);
+            });
+        }
+
+        return $this->cache(function () {
+            return $this->roomBoysQuery->paginate(10);
+        });
+    }
+
+    public function manageDesignation($room_boy_id)
+    {
+        $this->useCacheRows();
+        $this->designation = Designation::make([
+            'room_boy_id'=>$room_boy_id,
+            'current' => 1,
+        ]);
+
+        $this->room_boy = $this->roomBoys->find($room_boy_id);
+        $curren_designated_floors = $this->room_boy->designations()->where('current',1)->pluck('floor_id')->toArray();
+        $this->floors = Floor::where('branch_id', auth()->user()->branch_id)
+            ->whereNotIn('id',$curren_designated_floors)
+            ->get();
+
+        $this->manageDesignationModal = true;
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->room_boy->designations()->where('current',1)->update([
+            'current' => 0,
+        ]);
+
+        $this->designation->save();
+
+        $this->manageDesignationModal = false;
+
+        $this->notification()->success( 'Save successfully');
+
+    }
+
+    public function mount()
+    {
+        $this->floors_filter = Floor::where('branch_id', auth()->user()->branch_id)->get();
+    }
+
     public function render()
     {
-        return view('livewire.branch-admin.room-boy-designations');
+        return view('livewire.branch-admin.room-boy-designations',[
+            'roomBoys' => $this->roomBoys,
+        ]);
     }
 }
--- resources/views/livewire/branch-admin/room-boy-designations.blade.php
+++ resources/views/livewire/branch-admin/room-boy-designations.blade.php
@@ -1,3 +1,102 @@
 <div>
-    @livewire('housekeeping.designations')
+    <x-table :headers="['Name', 'Status', 'Cleaning', 'Floor Designation', '']">
+        <x-slot:topLeft>
+            <x-input placeholder="Search"
+                wire:model.debounce.500ms="search"
+                icon="search" />
+            <x-native-select wire:model="filter.floor_id">
+                <option value="">Select Floor (ALL)</option>
+                @foreach ($floors_filter as $item)
+                    <option value="{{ $item->id }}">
+                        {{ ordinal($item->number) }} Floor
+                    </option>
+                @endforeach
+            </x-native-select>
+        </x-slot:topLeft>
+        <x-slot:topRight>
+
+        </x-slot:topRight>
+        @forelse ($roomBoys as $roomBoy)
+            <x-table-row>
+                <x-table-data>
+                    {{ $roomBoy->user->name }}
+                </x-table-data>
+                <x-table-data>
+                    @if ($roomBoy->is_cleaning)
+                        <span
+                            class="inline-flex items-center rounded-full bg-green-100 px-2.5 py-0.5 text-xs font-medium text-green-800">
+                            Cleaning
+                        </span>
+                    @else
+                        <span
+                            class="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800">
+                            Not Cleaning
+                        </span>
+                    @endif
+                </x-table-data>
+                <x-table-data>
+                    @if ($roomBoy->is_cleaning)
+                        Currently Cleaning in Room # {{ $roomBoy->room->number }}
+                    @else
+                        <span
+                            class="inline-flex items-center rounded-full bg-red-100 px-2.5 py-0.5 text-xs font-medium text-red-800">
+                            Not Cleaning
+                        </span>
+                    @endif
+                </x-table-data>
+
+                <x-table-data>
+                    @forelse ($roomBoy->designations as $designation)
+                        {{ ordinal($designation->floor->number) }} Floor
+                    @empty
+                        No Designation
+                    @endforelse
+                </x-table-data>
+                <x-table-data>
+                    <div class="flex justify-end">
+                        <x-button sm
+                            wire:click="manageDesignation({{ $roomBoy->id }})"
+                            label="Manage Designation" />
+                    </div>
+                </x-table-data>
+            </x-table-row>
+        @empty
+            <x-table-empty rows="4" />
+        @endforelse
+        <x-slot:pagination>
+            {{ $roomBoys->links() }}
+        </x-slot:pagination>
+    </x-table>
+
+    <div id="modals">
+        <form>
+            @csrf
+            <x-modal.card wire:model.defer="manageDesignationModal">
+                <x-slot:title>
+                    Assign to floor
+                </x-slot:title>
+                <div>
+                    <x-native-select wire:model.defer="designation.floor_id"
+                        value=""
+                        label="Select Floor">
+                        <option value="">Select Floor</option>
+                        @foreach ($floors as $floor)
+                            <option value="{{ $floor->id }}">
+                                {{ ordinal($floor->number) }} Floor
+                            </option>
+                        @endforeach
+                    </x-native-select>
+                </div>
+                <x-slot:footer>
+                    <div class="flex justify-end space-x-3">
+                        <x-button x-on:click="close"
+                            label="Cancel" />
+                        <x-button wire:click="save"
+                            positive
+                            label="Save" />
+                    </div>
+                </x-slot:footer>
+            </x-modal.card>
+        </form>
+    </div>
 </div>
