6aa32c5bbc1dadb849fa1718b4c4529621b7db2c
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-01-06 00:10:03 +0800

    extend-rey-code

--- app/Http/Livewire/V2/FrontDesk/Transactions/Extend.php
+++ app/Http/Livewire/V2/FrontDesk/Transactions/Extend.php
@@ -6,6 +6,7 @@ use App\Http\Livewire\FrontDesk\CheckIn;
 use Livewire\Component;
 
 use Carbon\Carbon;
+use App\Models\Frontdesk;
 use App\Models\Extension;
 use App\Models\Transaction;
 use App\Models\CheckInDetail;
@@ -26,19 +27,28 @@ class Extend extends Component
 
     public $checkInDetailId;
 
-    public $checkInDetailRoomId,$checkInDetailRoomTypeId,$checkInDetailRoomRateId,$checkInDetailRoomRateAmount,$checkInDetailRoomRateStayingHour,$checkInDetailStaticHourStayed,$checkInDetailExpectedCheckOutAt;
+    public $checkInDetailRoomId,
+        $checkInDetailRoomTypeId,
+        $checkInDetailRoomRateId,
+        $checkInDetailRoomRateAmount,
+        $checkInDetailRoomRateStayingHour,
+        $checkInDetailStaticHourStayed,
+        $checkInDetailExpectedCheckOutAt;
 
-    public $branchResettingTime,$resettingTimeRateAmount;
+    public $branchResettingTime, $resettingTimeRateAmount;
 
     public $extensionRates = [];
 
     public $actionUnavailable = false;
 
-    public $extensionHour,$extensionAmount;
+    public $extensionHour, $extensionAmount;
 
-    protected $listeners = ['confirmExtension','payTransaction','depositDeducted'=>'$refresh'];
+    protected $listeners = [
+        'confirmExtension',
+        'payTransaction',
+        'depositDeducted' => '$refresh',
+    ];
 
-    
     public $transactionToPayAmount = 0;
     public $transactionToPayGivenAmount = 0;
     public $transactionToPayExcessAmount = 0;
@@ -57,8 +67,12 @@ class Extend extends Component
 
     public function updatedTransactionToPayGivenAmount()
     {
-        if ($this->transactionToPayGivenAmount > $this->transactionToPayAmount) {
-            $this->transactionToPayExcessAmount = $this->transactionToPayGivenAmount - $this->transactionToPayAmount;
+        if (
+            $this->transactionToPayGivenAmount > $this->transactionToPayAmount
+        ) {
+            $this->transactionToPayExcessAmount =
+                $this->transactionToPayGivenAmount -
+                $this->transactionToPayAmount;
         } else {
             $this->transactionToPayExcessAmount = 0;
         }
@@ -67,31 +81,41 @@ class Extend extends Component
     public function payTransactionConfirm()
     {
         $ids = json_decode(auth()->user()->assigned_frontdesks);
-        $frontdesks = Frontdesk::whereIn('id',$ids)->get();
-        if ($this->transactionToPayGivenAmount < $this->transactionToPayAmount) {
+        $frontdesks = Frontdesk::whereIn('id', $ids)->get();
+        if (
+            $this->transactionToPayGivenAmount < $this->transactionToPayAmount
+        ) {
             $this->dispatchBrowserEvent('show-alert', [
                 'type' => 'error',
                 'title' => 'Invalid Amount',
-                 'message' => 'Given amount is less than the payable amount.'
-             ]);
-             return;
+                'message' => 'Given amount is less than the payable amount.',
+            ]);
+            return;
         }
 
         if ($this->transactionToPayExcessAmount) {
             Deposit::create([
                 'guest_id' => $this->guestId,
                 'amount' => $this->transactionToPayExcessAmount,
-                'remarks' => 'Excess amount from transaction :'.$this->transactionToPay->remarks,
-                'remaining'=> $this->transactionToPayExcessAmount,
-                'front_desk_names' => $frontdesks->pluck('name')->implode(' and '),
+                'remarks' =>
+                    'Excess amount from transaction :' .
+                    $this->transactionToPay->remarks,
+                'remaining' => $this->transactionToPayExcessAmount,
+                'front_desk_names' => $frontdesks
+                    ->pluck('name')
+                    ->implode(' and '),
             ]);
 
             $guest = Guest::find($this->guestId);
             $guest->update([
-                'total_deposits' => $guest->total_deposits + $this->transactionToPayExcessAmount,
-                'deposit_balance' => $guest->deposit_balance + $this->transactionToPayExcessAmount,
+                'total_deposits' =>
+                    $guest->total_deposits +
+                    $this->transactionToPayExcessAmount,
+                'deposit_balance' =>
+                    $guest->deposit_balance +
+                    $this->transactionToPayExcessAmount,
             ]);
-        };
+        }
 
         $this->transactionToPay->update([
             'paid_at' => Carbon::now(),
@@ -101,48 +125,56 @@ class Extend extends Component
         $this->dispatchBrowserEvent('notify-alert', [
             'type' => 'success',
             'title' => 'Transaction Paid',
-            'message' => 'Transaction has been paid.'
+            'message' => 'Transaction has been paid.',
         ]);
 
         $this->emit('transactionUpdated');
     }
 
-
-    public function payWithDeposit($transaction_id,$payable_amount)
+    public function payWithDeposit($transaction_id, $payable_amount)
     {
-        $this->emit('payWithDeposit',[
+        $this->emit('payWithDeposit', [
             'guest_id' => $this->guestId,
             'transaction_id' => $transaction_id,
-            'payable_amount' => $payable_amount
+            'payable_amount' => $payable_amount,
         ]);
     }
 
     public function mount()
     {
-       $this->extensionRates = Extension::where('branch_id', auth()->user()->branch_id)->get();
-       if(count($this->extensionRates) == 0){
-           $this->actionUnavailable = true;
-           return;
-       }
-
-       $extension_capping = ExtensionCapping::where('branch_id', auth()->user()->branch_id)->first();
-       if($extension_capping == null){
+        $this->extensionRates = Extension::where(
+            'branch_id',
+            auth()->user()->branch_id
+        )->get();
+        if (count($this->extensionRates) == 0) {
             $this->actionUnavailable = true;
             return;
-       }else{
-              $this->branchResettingTime = $extension_capping->hours;
-              $rateForResettingTime =  Rate::where('branch_id', auth()->user()->branch_id)
-              ->whereHas('staying_hour', function ($query) {
-                  $query->where('number', $this->branchResettingTime);
-              })->first();
+        }
 
-              if ($rateForResettingTime == null) {
-                    $this->actionUnavailable = true;
-                    return;
-              }
-              $this->resettingTimeRateAmount = $rateForResettingTime->amount;
-       }
+        $extension_capping = ExtensionCapping::where(
+            'branch_id',
+            auth()->user()->branch_id
+        )->first();
+        if ($extension_capping == null) {
+            $this->actionUnavailable = true;
+            return;
+        } else {
+            $this->branchResettingTime = $extension_capping->hours;
+            $rateForResettingTime = Rate::where(
+                'branch_id',
+                auth()->user()->branch_id
+            )
+                ->whereHas('staying_hour', function ($query) {
+                    $query->where('number', $this->branchResettingTime);
+                })
+                ->first();
 
+            if ($rateForResettingTime == null) {
+                $this->actionUnavailable = true;
+                return;
+            }
+            $this->resettingTimeRateAmount = $rateForResettingTime->amount;
+        }
     }
 
     public function updatedExtensionHour()
@@ -151,50 +183,81 @@ class Extend extends Component
         $extensionRate = $this->extensionRates->find($this->extensionHour);
 
         // to get the sum of hours extended by the guest
-        $extensionHistory = StayExtension::where('guest_id',$this->guestId);
+        $extensionHistory = StayExtension::where('guest_id', $this->guestId);
         $totalExtensionHours = $extensionHistory->sum('hours');
-        $totalHours = $this->checkInDetailStaticHourStayed + $totalExtensionHours;
+        $totalHours =
+            $this->checkInDetailStaticHourStayed + $totalExtensionHours;
 
         $resetHours = StayingHour::where('branch_id', auth()->user()->branch_id)
             ->where('number', '>=', $extensionRate->hours)
             ->orderBy('number', 'ASC')
-            ->first()->rates()->where('type_id', $this->checkInDetailRoomTypeId)->first()->amount;
+            ->first()
+            ->rates()
+            ->where('type_id', $this->checkInDetailRoomTypeId)
+            ->first()->amount;
 
         if ($totalHours % $this->branchResettingTime == 0) {
             if ($extensionRate->hours < $this->branchResettingTime) {
                 $this->extensionAmount = $resetHours;
             } else {
-                $days = floor($extensionRate->hours / $this->branchResettingTime);
+                $days = floor(
+                    $extensionRate->hours / $this->branchResettingTime
+                );
                 $hours = $extensionRate->hours % $this->branchResettingTime;
                 $dailyAmount = $this->resettingTimeRateAmount * $days;
-                $hourlyRateAmount = $hours > 0 ? Rate::where('type_id', $this->checkInDetailRoomTypeId)
-                    ->whereHas('staying_hour', function ($query) use ($hours) {
-                        $query->where('number', '>=', $hours);
-                    })->first()->amount : 0;
-                $totalExtendAmount = StayExtension::whereHas('transaction', function ($query) {
-                    $query->where('guest_id', $this->guestId);
-                })->sum('amount');
-                $totalCheckedInAmountAndExtensionAmount = $this->checkInDetailRoomRateAmount + $totalExtendAmount;
+                $hourlyRateAmount =
+                    $hours > 0
+                        ? Rate::where('type_id', $this->checkInDetailRoomTypeId)
+                            ->whereHas('staying_hour', function ($query) use (
+                                $hours
+                            ) {
+                                $query->where('number', '>=', $hours);
+                            })
+                            ->first()->amount
+                        : 0;
+                $totalExtendAmount = StayExtension::whereHas(
+                    'transaction',
+                    function ($query) {
+                        $query->where('guest_id', $this->guestId);
+                    }
+                )->sum('amount');
+                $totalCheckedInAmountAndExtensionAmount =
+                    $this->checkInDetailRoomRateAmount + $totalExtendAmount;
                 $total_amount = $dailyAmount + $hourlyRateAmount;
-                $this->extensionAmount =  $total_amount - $totalCheckedInAmountAndExtensionAmount;
+                $this->extensionAmount =
+                    $total_amount - $totalCheckedInAmountAndExtensionAmount;
             }
         } else {
-            if ($extensionRate->hours + $totalHours <= $this->branchResettingTime) {
-                $this->extensionAmount =  $extensionRate->amount;
+            if (
+                $extensionRate->hours + $totalHours <=
+                $this->branchResettingTime
+            ) {
+                $this->extensionAmount = $extensionRate->amount;
             } else {
                 $temp_total = $extensionRate->hours + $totalHours;
                 $days = floor($temp_total / $this->branchResettingTime);
                 $hours = $temp_total % $this->branchResettingTime;
                 $dailyAmount = $this->resettingTimeRateAmount * $days;
-                $hourlyRateAmount =  $hours > 0 ? Rate::where('type_id', $this->checkInDetailRoomTypeId)
-                    ->whereHas('staying_hour', function ($query) use ($hours) {
-                        $query->where('number', '>=', $hours);
-                    })->first()->amount : 0;
-                $totalExtendAmount = StayExtension::where('guest_id', $this->guestId)->sum('amount');
+                $hourlyRateAmount =
+                    $hours > 0
+                        ? Rate::where('type_id', $this->checkInDetailRoomTypeId)
+                            ->whereHas('staying_hour', function ($query) use (
+                                $hours
+                            ) {
+                                $query->where('number', '>=', $hours);
+                            })
+                            ->first()->amount
+                        : 0;
+                $totalExtendAmount = StayExtension::where(
+                    'guest_id',
+                    $this->guestId
+                )->sum('amount');
 
-                $totalCheckedInAmountAndExtensionAmount = $this->checkInDetailRoomRateAmount + $totalExtendAmount;
+                $totalCheckedInAmountAndExtensionAmount =
+                    $this->checkInDetailRoomRateAmount + $totalExtendAmount;
                 $total_amount = $dailyAmount + $hourlyRateAmount;
-                $this->extensionAmount =  $total_amount - $totalCheckedInAmountAndExtensionAmount;
+                $this->extensionAmount =
+                    $total_amount - $totalCheckedInAmountAndExtensionAmount;
             }
         }
         DB::commit();
@@ -208,18 +271,29 @@ class Extend extends Component
 
         DB::beginTransaction();
         $ids = json_decode(auth()->user()->assigned_frontdesks);
-        $frontdesks = Frontdesk::whereIn('id',$ids)->get();
-        $extensionHours = $this->extensionRates->find($this->extensionHour)->hours;
+        $active_frontdesk = Frontdesk::where(
+            'branch_id',
+            auth()->user()->branch_id
+        )
+            ->where('is_active', 1)
+            ->pluck('id');
+
+        $frontdesks = Frontdesk::whereIn('id', $active_frontdesk)->get();
+        // dd($frontdesks);
+        $extensionHours = $this->extensionRates->find($this->extensionHour)
+            ->hours;
         $extension_transaction = Transaction::create([
             'guest_id' => $this->guestId,
             'branch_id' => auth()->user()->branch_id,
             'transaction_type_id' => 6,
             'payable_amount' => $this->extensionAmount,
             'room_id' => $this->checkInDetailRoomId,
-            'remarks' => 'Guest extended his/her stay for ' . $extensionHours . ' hours',
-            'assigned_frontdesks' => auth()->user()->assigned_frontdesks,
+            'remarks' =>
+                'Guest extended his/her stay for ' . $extensionHours . ' hours',
+            // 'assigned_frontdesks' => auth()->user()->assigned_frontdesks,  //jam code
+            'assigned_frontdesks' => $active_frontdesk, //rey code
         ]);
-        
+
         StayExtension::create([
             'guest_id' => $this->guestId,
             'extension_id' => $this->extensionHour,
@@ -229,13 +303,15 @@ class Extend extends Component
         ]);
 
         CheckInDetail::where('guest_id', $this->guestId)->update([
-            'expected_check_out_at' => Carbon::parse($this->checkInDetailExpectedCheckOutAt)->addHours($extensionHours)
+            'expected_check_out_at' => Carbon::parse(
+                $this->checkInDetailExpectedCheckOutAt
+            )->addHours($extensionHours),
         ]);
 
         DB::commit();
 
-        $this->extensionHour = "";
-        $this->extensionAmount = "";
+        $this->extensionHour = '';
+        $this->extensionAmount = '';
 
         $this->emit('transactionUpdated');
 
@@ -244,16 +320,18 @@ class Extend extends Component
         $this->dispatchBrowserEvent('notify-alert', [
             'type' => 'success',
             'title' => 'Success',
-            'message' => 'Guest stay extended successfully!'
+            'message' => 'Guest stay extended successfully!',
         ]);
     }
 
-
     public function getTransactionsQueryProperty()
     {
-        return Transaction::where('guest_id', $this->guestId)->where('transaction_type_id', 6);
+        return Transaction::where('guest_id', $this->guestId)->where(
+            'transaction_type_id',
+            6
+        );
     }
-    
+
     public function getTransactionsProperty()
     {
         return $this->cache(function () {
@@ -263,7 +341,7 @@ class Extend extends Component
 
     public function render()
     {
-        return view('livewire.v2.front-desk.transactions.extend',[
+        return view('livewire.v2.front-desk.transactions.extend', [
             'transactions' => $this->transactions,
         ]);
     }
--- resources/views/livewire/v2/front-desk/assign-front-desk.blade.php
+++ resources/views/livewire/v2/front-desk/assign-front-desk.blade.php
@@ -1,102 +1,99 @@
 <div>
-
-    <div class="mb-10">
-        <h1 class="text-center text-2xl font-semibold uppercase">
-            Front Desk Shift Assignment
-        </h1>
-        <div class="mt-3 rounded-md border border-yellow-400 bg-yellow-50 p-4">
-            <div class="flex">
-                <div class="flex-shrink-0">
-                    <!-- Heroicon name: mini/exclamation-triangle -->
-                    <svg class="h-5 w-5 text-yellow-400"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true">
-                        <path fill-rule="evenodd"
-                            d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z"
-                            clip-rule="evenodd" />
-                    </svg>
-                </div>
-                <div class="ml-3">
-                    <h3 class="text-sm font-medium text-yellow-800">
-                        No front desk assigned
-                    </h3>
-                    <div class="mt-2 text-sm text-yellow-700">
-                        <p>
-                            Please assign a front desk to start the shift. Select as much as applicable.
-                        </p>
-                    </div>
-                </div>
-            </div>
+  <div class="fixed top-10 right-10">
+    <form method="POST" action="{{ route('logout') }}">
+      @csrf
+      <x-button href="{{ route('logout') }}" onclick="event.preventDefault();
+    this.closest('form').submit();"
+        label="Logout" icon="logout" slate />
+    </form>
+  </div>
+  <div class="mb-10">
+    <h1 class="text-center text-2xl font-semibold uppercase">
+      Front Desk Shift Assignment
+    </h1>
+    <div class="mt-3 rounded-md border border-yellow-400 bg-yellow-50 p-4">
+      <div class="flex">
+        <div class="flex-shrink-0">
+          <!-- Heroicon name: mini/exclamation-triangle -->
+          <svg class="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            fill="currentColor" aria-hidden="true">
+            <path fill-rule="evenodd"
+              d="M8.485 3.495c.673-1.167 2.357-1.167 3.03 0l6.28 10.875c.673 1.167-.17 2.625-1.516 2.625H3.72c-1.347 0-2.189-1.458-1.515-2.625L8.485 3.495zM10 6a.75.75 0 01.75.75v3.5a.75.75 0 01-1.5 0v-3.5A.75.75 0 0110 6zm0 9a1 1 0 100-2 1 1 0 000 2z"
+              clip-rule="evenodd" />
+          </svg>
         </div>
+        <div class="ml-3">
+          <h3 class="text-sm font-medium text-yellow-800">
+            No front desk assigned
+          </h3>
+          <div class="mt-2 text-sm text-yellow-700">
+            <p>
+              Please assign a front desk to start the shift. Select as much as applicable.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div>
+    <div>
+      <x-my.input label="Select your Name" wire:model.debounce.500ms="search" placeholder="Search" />
     </div>
     <div>
-        <div>
-            <x-my.input label="Select your Name"
-                wire:model.debounce.500ms="search"
-                placeholder="Search" />
-        </div>
-        <div>
-            <div class="mt-6 flow-root">
-                <ul role="list"
-                    class="-my-5 divide-y divide-gray-200">
-                    @foreach ($frontdesks as $frontdesk)
-                        <li wire:key='ff{{ $frontdesk->id }}'
-                            class="py-4">
-                            <div class="flex items-center space-x-4">
-                                <div class="min-w-0 flex-1">
-                                    <p class="truncate text-sm font-medium text-gray-900">
-                                        {{ $frontdesk->name }}
-                                    </p>
-                                    <p class="truncate text-sm text-gray-500">
-                                        {{ $frontdesk->contact_number }}
-                                    </p>
-                                </div>
-                                <div>
-                                    <button type="button"
-                                        wire:click="select({{ $frontdesk->id }},'{{ $frontdesk->name }}')"
-                                        class="inline-flex items-center rounded-full border border-gray-300 bg-white px-2.5 py-0.5 text-sm font-medium leading-5 text-gray-700 shadow-sm hover:bg-gray-50">
-                                        Select
-                                    </button>
-                                </div>
-                            </div>
-                        </li>
-                    @endforeach
-                </ul>
-            </div>
-        </div>
+      <div class="mt-6 flow-root">
+        <ul role="list" class="-my-5 divide-y divide-gray-200">
+          @foreach ($frontdesks as $frontdesk)
+            <li wire:key='ff{{ $frontdesk->id }}' class="py-4">
+              <div class="flex items-center space-x-4">
+                <div class="min-w-0 flex-1">
+                  <p class="truncate text-sm font-medium text-gray-900">
+                    {{ $frontdesk->name }}
+                  </p>
+                  <p class="truncate text-sm text-gray-500">
+                    {{ $frontdesk->contact_number }}
+                  </p>
+                </div>
+                <div>
+                  <button type="button" wire:click="select({{ $frontdesk->id }},'{{ $frontdesk->name }}')"
+                    class="inline-flex items-center rounded-full border border-gray-300 bg-white px-2.5 py-0.5 text-sm font-medium leading-5 text-gray-700 shadow-sm hover:bg-gray-50">
+                    Select
+                  </button>
+                </div>
+              </div>
+            </li>
+          @endforeach
+        </ul>
+      </div>
     </div>
-    <div wire:key="selected"
-        class="mt-20">
-        @if (count($selecteds) > 0)
+  </div>
+  <div wire:key="selected" class="mt-20">
+    @if (count($selecteds) > 0)
+      <div>
+        <h1 class="mb-3 text-center text-xl">
+          Selected Front Desks
+        </h1>
+        <div class="space-y-3 p-2">
+          @forelse ($selecteds as $key => $selected)
+            <div wire:key="xx{{ $key }}"
+              class="flex justify-between rounded-lg border bg-white px-3 py-2 shadow">
+              <span>
+                {{ $selected }}
+              </span>
+            </div>
+          @empty
             <div>
-                <h1 class="mb-3 text-center text-xl">
-                    Selected Front Desks
-                </h1>
-                <div class="space-y-3 p-2">
-                    @forelse ($selecteds as $key => $selected)
-                        <div wire:key="xx{{ $key }}"
-                            class="flex justify-between rounded-lg border bg-white px-3 py-2 shadow">
-                            <span>
-                                {{ $selected }}
-                            </span>
-                        </div>
-                    @empty
-                        <div>
-                            <span>
-                                Please select a front desk
-                            </span>
-                        </div>
-                    @endforelse
-                </div>
-                <div class="buttons mt-3 flex justify-center">
-                    <x-button positive
-                        wire:click="startShift">
-                        Start Shift
-                    </x-button>
-                </div>
+              <span>
+                Please select a front desk
+              </span>
             </div>
-        @endif
-    </div>
+          @endforelse
+        </div>
+        <div class="buttons mt-3 flex justify-center">
+          <x-button positive wire:click="startShift">
+            Start Shift
+          </x-button>
+        </div>
+      </div>
+    @endif
+  </div>
 </div>
