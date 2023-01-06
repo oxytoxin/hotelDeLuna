7436c6a3538449115d304c6d06c11823b6faf59c
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-01-06 09:26:17 +0800

    deposit extend

--- resources/views/livewire/v2/front-desk/transactions/transactions.blade.php
+++ resources/views/livewire/v2/front-desk/transactions/transactions.blade.php
@@ -1,63 +1,69 @@
 <div>
-    <div class="px-4 pt-4 sm:px-6 lg:px-8">
-        <div class="sm:flex sm:items-center">
-            <div class="sm:flex-auto">
-                <h1 class="text-lg font-semibold text-gray-900">
-                    Transactions
-                </h1>
-            </div>
-        </div>
-        <div class="mt-3 flex flex-col">
-            <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="inline-block min-w-full py-2 align-middle">
-                    <div class="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
-                        <table class="min-w-full divide-y divide-gray-300">
-                            <thead class="bg-gray-50">
-                                <tr>
-                                    <th scope="col"
-                                        class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">
-                                        Details
-                                    </th>
-                                    <th scope="col"
-                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Amount
-                                    </th>
-                                    <th scope="col"
-                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Transaction Date
-                                    </th>
-                                    <th scope="col"
-                                        class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                                        Paid at
-                                    </th>
-                                </tr>
-                            </thead>
-                            <tbody class="divide-y divide-gray-200 bg-white">
-                                @foreach ($guest_transactions as $transaction_type_id => $transactions)
-                                    <tr class="border-t border-gray-200">
-                                        <th colspan="5"
-                                            scope="colgroup"
-                                            class="bg-primary-600 px-4 py-2 text-left text-sm font-semibold text-white sm:px-6">
-                                            {{ $transactionTypes->find($transaction_type_id)->name }}
-                                        </th>
-                                    </tr>
-                                    @foreach ($transactions as $transaction)
-                                        <tr>
-                                            <td class="py-3.5 pl-4 pr-3 text-xs text-gray-900 sm:pl-6 lg:pl-8">
-                                                {{ $transaction->remarks }}
-                                            </td>
-                                            <td class="px-3 py-3.5 text-xs text-gray-900">
-                                                {{ $transaction->payable_amount }}
-                                            </td>
+  <div class="px-4 pt-4 sm:px-6 lg:px-8">
+    <div class="sm:flex sm:items-center">
+      <div class="sm:flex-auto">
+        <h1 class="text-lg font-semibold text-gray-900">
+          Transactions
+        </h1>
+      </div>
+    </div>
+    <div class="mt-3 flex flex-col">
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="inline-block min-w-full py-2 align-middle">
+          <div class="overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5">
+            <table class="min-w-full divide-y divide-gray-300">
+              <thead class="bg-gray-50">
+                <tr>
+                  <th scope="col"
+                    class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:pl-8">
+                    Details
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Amount
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Transaction Date
+                  </th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    Paid at
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="divide-y divide-gray-200 bg-white">
+                @foreach ($guest_transactions as $transaction_type_id => $transactions)
+                  <tr class="border-t border-gray-200">
+                    <th colspan="5" scope="colgroup"
+                      class="bg-primary-600 px-4 py-2 text-left text-sm font-semibold text-white sm:px-6">
+                      {{ $transactionTypes->find($transaction_type_id)->name }}
+                    </th>
+                  </tr>
+                  @foreach ($transactions as $transaction)
+                    <tr>
+                      <td class="py-3.5 pl-4 pr-3 text-xs text-gray-900 sm:pl-6 lg:pl-8">
+                        @php
+                          $text = $transaction->remarks;
+                          $remarks = strpos($text, ':');
+                          
+                          $check_in = substr($text, 0, $remarks + 1);
+                          $details = substr($text, $remarks + 2);
+                          
+                        @endphp
+                        <h1>{{ $check_in }}</h1>
+                        <h1>{{ $details }}</h1>
 
-                                            <td class="px-3 py-3.5 text-xs text-gray-900">
-                                                {{ $transaction->created_at->format('M d, Y h:i A') }}
-                                            </td>
-                                            <td class="px-3 py-3.5 text-right text-xs font-medium">
-                                                <div class="flex space-x-3">
-                                                    @if ($transaction->paid_at == null)
-                                                        <x-my.button-success label="Pay"
-                                                            x-on:click="$dispatch('confirm',{
+                      </td>
+                      <td class="px-3 py-3.5 text-xs text-gray-900">
+                        {{ $transaction->payable_amount }}
+                      </td>
+
+                      <td class="px-3 py-3.5 text-xs text-gray-900">
+                        {{ $transaction->created_at->format('M d, Y h:i A') }}
+                      </td>
+                      <td class="px-3 py-3.5 text-right text-xs font-medium">
+                        <div class="flex space-x-3">
+                          @if ($transaction->paid_at == null)
+                            <x-my.button-success label="Pay"
+                              x-on:click="$dispatch('confirm',{
                                                         title : 'Are you sure?',
                                                         message : 'Are you sure you want to proceed?',
                                                         confirmButtonText : 'Confirm',
@@ -65,20 +71,20 @@
                                                         confirmMethod : 'payTransaction',
                                                         confirmParams :{{ $transaction->id }},
                                                 })"
-                                                            py="py-1" />
-                                                    @else
-                                                        {{ Carbon\Carbon::parse($transaction->paid_at)->format('M d, Y h:i A') }}
-                                                    @endif
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+                              py="py-1" />
+                          @else
+                            {{ Carbon\Carbon::parse($transaction->paid_at)->format('M d, Y h:i A') }}
+                          @endif
+                        </div>
+                      </td>
+                    </tr>
+                  @endforeach
+                @endforeach
+              </tbody>
+            </table>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </div>
--- resources/views/livewire/v2/pay-with-deposits.blade.php
+++ resources/views/livewire/v2/pay-with-deposits.blade.php
@@ -1,54 +1,45 @@
 <div>
-    <x-my.modal title="Select Deposits"
-        width="sm:max-w-xl"
-        :showOn="['show-deposits-modal']"
-        :closeOn="['close-deposits-modal']">
-        @if ($guest)
-            <div class="grid gap-4">
-                <div class="">
-                    Total Balance: ₱ {{ $guest->deposit_balance }}
+  <x-my.modal title="Select Deposits" width="sm:max-w-xl" :showOn="['show-deposits-modal']" :closeOn="['close-deposits-modal']">
+    @if ($guest)
+      <div class="grid gap-4">
+        <div class="">
+          Total Balance: ₱ {{ $guest->deposit_balance }}
+        </div>
+        <div class="">
+          Total Amount to Pay: ₱ {{ $payableAmount }}
+        </div>
+        @if ($guest->deposit_balance < $payableAmount)
+          <div class="">
+            <span class="text-sm text-blue-500">Note: Deposit amount is not enough. Please enter the amount in
+              addition to the total balance.</span>
+            <x-my.input type="number" label="" wire:model.debounce.500ms="additionalAmount" />
+          </div>
+          <div class="">
+            @if ($additionalAmountChange >= 0)
+              @if ($additionalAmountChange > 0)
+                <span> Excess Amount : ₱ {{ $additionalAmountChange }}</span>
+                <div class="flex items-center space-x-3">
+                  <input id="deposit" aria-describedby="comments-description" name="deposit" type="checkbox"
+                    wire:model.defer="additionalAmountChangeSaveToDeposit"
+                    class="w-4 h-4 border-gray-300 rounded text-primary-600 focus:ring-primary-500">
+                  <span>
+                    Save excess amount as deposit
+                  </span>
                 </div>
-                <div class="">
-                    Total Amount to Pay: ₱ {{ $payableAmount }}
-                </div>
-                @if ($guest->deposit_balance < $payableAmount)
-                    <div class="">
-                        <span class="text-sm text-red-500">Deposit amount is not enough. Please enter the amount in
-                            addition to the total balance.</span>
-                        <x-my.input type="number"
-                            label=""
-                            wire:model.debounce.500ms="additionalAmount" />
-                    </div>
-                    <div class="">
-                        @if ($additionalAmountChange >= 0)
-                            @if ($additionalAmountChange > 0)
-                                <span> Excess Amount : ₱ {{ $additionalAmountChange }}</span>
-                                <div class="flex items-center space-x-3">
-                                    <input id="deposit"
-                                        aria-describedby="comments-description"
-                                        name="deposit"
-                                        type="checkbox"
-                                        wire:model.defer="additionalAmountChangeSaveToDeposit"
-                                        class="w-4 h-4 border-gray-300 rounded text-primary-600 focus:ring-primary-500">
-                                    <span>
-                                        Save excess amount as deposit
-                                    </span>
-                                </div>
-                            @endif
-                        @else
-                            <span class="text-sm text-red-500">
-                                Invalid amount.
-                            </span>
-                        @endif
-                    </div>
-                @endif
-            </div>
+              @endif
+            @else
+              <span class="text-sm text-red-500">
+                Invalid amount.
+              </span>
+            @endif
+          </div>
         @endif
-        <x-slot:footer>
-            <div class="flex space-x-3">
-                <x-my.button-success wire:click="save"
-                    label="Pay" />
-            </div>
-        </x-slot:footer>
-    </x-my.modal>
+      </div>
+    @endif
+    <x-slot:footer>
+      <div class="flex space-x-3">
+        <x-my.button-success wire:click="save" label="Pay" />
+      </div>
+    </x-slot:footer>
+  </x-my.modal>
 </div>
