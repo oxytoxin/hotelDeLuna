2465c29e131b41e1a21cd70e45dcaaf15866efc8
johnreynaceda <johnreynaceda@sksu.edu.ph>
2023-01-07 08:52:57 +0800

    food/amenities

--- app/Http/Livewire/V2/FrontDesk/Transactions/FoodAndBeverage.php
+++ app/Http/Livewire/V2/FrontDesk/Transactions/FoodAndBeverage.php
@@ -6,6 +6,8 @@ use Livewire\Component;
 use App\Models\Frontdesk;
 use App\Models\Transaction;
 use App\Traits\WithCaching;
+use App\Models\Deposit;
+use App\Models\Guest;
 use Illuminate\Support\Facades\DB;
 use App\Models\FoodAndBeverage as ModelFoodAndBeverage;
 
@@ -124,6 +126,12 @@ class FoodAndBeverage extends Component
 
     public $foodAndBeverages;
 
+    public $transactionToPay;
+    public $transactionToPayAmount = 0;
+    public $transactionToPayGivenAmount = 0;
+    public $transactionToPayExcessAmount = 0;
+    public $transactionToPaySaveExcessAmount = false;
+
     protected $listeners = [
         'confirmSaveRecord',
         'payTransaction',
@@ -141,17 +149,99 @@ class FoodAndBeverage extends Component
 
     public function payTransaction(Transaction $transaction)
     {
-        $transaction->update([
+        $this->transactionToPay = $transaction;
+
+        $this->transactionToPayAmount = $transaction->payable_amount;
+        $this->transactionToPayGivenAmount = 0;
+        $this->transactionToPayExcessAmount = 0;
+
+        $this->dispatchBrowserEvent('show-pay-modal');
+    }
+
+    public function updatedTransactionToPayGivenAmount()
+    {
+        if (
+            $this->transactionToPayGivenAmount > $this->transactionToPayAmount
+        ) {
+            $this->transactionToPayExcessAmount =
+                $this->transactionToPayGivenAmount -
+                $this->transactionToPayAmount;
+        } else {
+            $this->transactionToPayExcessAmount = 0;
+        }
+    }
+
+    public function payTransactionConfirm()
+    {
+        if (
+            $this->transactionToPayGivenAmount < $this->transactionToPayAmount
+        ) {
+            $this->dispatchBrowserEvent('show-alert', [
+                'type' => 'error',
+                'title' => 'Invalid Amount',
+                'message' => 'Given amount is less than the payable amount.',
+            ]);
+            return;
+        }
+
+        if ($this->transactionToPayExcessAmount) {
+            $ids = json_decode(auth()->user()->assigned_frontdesks);
+            $active_frontdesk = Frontdesk::where(
+                'branch_id',
+                auth()->user()->branch_id
+            )
+                ->where('is_active', 1)
+                ->get();
+            Deposit::create([
+                'guest_id' => $this->guestId,
+                'amount' => $this->transactionToPayExcessAmount,
+                'remarks' =>
+                    'Excess amount from transaction :' .
+                    $this->transactionToPay->remarks,
+                'remaining' => $this->transactionToPayExcessAmount,
+                'front_desk_names' => $active_frontdesk
+                    ->pluck('name')
+                    ->implode(' and '),
+            ]);
+
+            $guest = Guest::find($this->guestId);
+            $guest->update([
+                'total_deposits' =>
+                    $guest->total_deposits +
+                    $this->transactionToPayExcessAmount,
+                'deposit_balance' =>
+                    $guest->deposit_balance +
+                    $this->transactionToPayExcessAmount,
+            ]);
+        }
+
+        $this->transactionToPay->update([
             'paid_at' => now(),
         ]);
-        $this->emit('transactionUpdated');
 
+        $this->dispatchBrowserEvent('close-pay-modal');
         $this->dispatchBrowserEvent('notify-alert', [
             'type' => 'success',
             'title' => 'Transaction Paid',
-            'message' => 'Transaction has been paid',
+            'message' => 'Transaction has been paid.',
         ]);
+
+        $this->emit('transactionUpdated');
     }
+
+    // public function payTransaction(Transaction $transaction)
+    // {
+    //     $transaction->update([
+    //         'paid_at' => now(),
+    //     ]);
+    //     $this->emit('transactionUpdated');
+
+    //     $this->dispatchBrowserEvent('notify-alert', [
+    //         'type' => 'success',
+    //         'title' => 'Transaction Paid',
+    //         'message' => 'Transaction has been paid',
+    //     ]);
+    // }
 
     public function rules()
     {
@@ -167,11 +257,18 @@ class FoodAndBeverage extends Component
     public function makeForm()
     {
         $ids = json_decode(auth()->user()->assigned_frontdesks);
-        $frontdesks = Frontdesk::whereIn('id', $ids)->get();
+        $active_frontdesk = Frontdesk::where(
+            'branch_id',
+            auth()->user()->branch_id
+        )
+            ->where('is_active', 1)
+            ->get();
         $this->form = ModelFoodAndBeverage::make([
             'guest_id' => $this->guestId,
             'quantity' => 1,
-            'front_desk_names' => $frontdesks->pluck('name')->implode(' and '),
+            'front_desk_names' => $active_frontdesk
+                ->pluck('name')
+                ->implode(' and '),
         ]);
     }
 
@@ -198,7 +295,12 @@ class FoodAndBeverage extends Component
     public function confirmSaveRecord()
     {
         $this->validate();
-
+        $active_frontdesk = Frontdesk::where(
+            'branch_id',
+            auth()->user()->branch_id
+        )
+            ->where('is_active', 1)
+            ->get();
         DB::beginTransaction();
 
         Transaction::create([
@@ -208,7 +310,7 @@ class FoodAndBeverage extends Component
             'room_id' => $this->guestCheckInRoomId,
             'payable_amount' => $this->form->price * $this->form->quantity,
             'remarks' => $this->form->name,
-            'assigned_frontdesks' => auth()->user()->assigned_frontdesks,
+            'assigned_frontdesks' => $active_frontdesk->pluck('id'),
         ]);
 
         $this->form->save();
